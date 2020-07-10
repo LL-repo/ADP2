b770034e0f6d839395ed998994fe10dc8dc6d8f3
Łukasz Łeszega <w60162@student.wsiz.rzeszow.pl>
2020-07-10 21:55:16 +0200

    Testy jednostkowe

---
+++ WindowsFormsApp1Tests/Form1Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp1;
+
+
+namespace WindowsFormsApp1.Tests
+{
+    [TestClass()]
+    public class Form1Tests
+    {
+        [TestMethod()]
+        public void SprawdzCzyRowneTest()
+        {
+            Walidator walidator = new Walidator();
+
+            bool wynikMetody = walidator.SprawdzCzyRowne("test", "test");
+            
+            Assert.AreEqual(wynikMetody, true);
+        }
+        
+        [TestMethod()]
+        public void DajZakonczenieTest()
+        {
+            Zakonczenia zakonczenia = new Zakonczenia();
+
+            string wynik = zakonczenia.DajZakonczenie(2);
+            
+            Assert.AreEqual(wynik, "Częśc języka polskiego zakończona");
+        }
+    }
+}
